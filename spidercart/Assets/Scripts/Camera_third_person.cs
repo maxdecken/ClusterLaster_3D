a45e2381c0b048b3566626464cfc1a6079eaec1f
COLIN_GARMS <colin.garms@online.de>
2023-01-05 19:36:18 +0100

    camera in main Scene

--- spidercart/Assets/Scripts/Camera_third_person.cs
+++ spidercart/Assets/Scripts/Camera_third_person.cs
@@ -18,7 +18,7 @@ public class Camera_third_person : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -27,6 +27,7 @@ public class Camera_third_person : MonoBehaviour
         camPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         Vector3 viewDirection = lookAt.position - camPosition;
         orientation.forward = viewDirection.normalized;
+        transform.position = player.position - player.transform.forward * 10;
 
         //playerObject.forward = viewDirection.normalized;
     }
